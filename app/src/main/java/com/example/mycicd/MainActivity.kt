391dccdecec9fb8827b4e09cbeb8c85eb5653ebe
christian111 <christian@rightchoice.email>
2024-01-17 11:42:48 +0800

    Add feature 1

--- app/src/main/java/com/example/mycicd/MainActivity.kt
+++ app/src/main/java/com/example/mycicd/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mycicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button : Button
+    lateinit var editText1: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            editText1 = findViewById(R.id.editTextText)
+            button = findViewById<Button?>(R.id.button)
+
+                button.setOnClickListener{
+                editText1.setText("testees")
+            }
     }
+
+
 }
