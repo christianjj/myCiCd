423284be813359c29ca8ae88695a426695b1b2a3
christian111 <christian@rightchoice.email>
2024-01-17 13:03:07 +0800

    allign

--- app/src/main/java/com/example/mycicd/MainActivity.kt
+++ app/src/main/java/com/example/mycicd/MainActivity.kt
@@ -7,18 +7,18 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button : Button
+    lateinit var button: Button
     lateinit var editText1: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            editText1 = findViewById(R.id.editTextText)
-            button = findViewById<Button?>(R.id.button)
+        editText1 = findViewById(R.id.editTextText)
+        button = findViewById<Button?>(R.id.button)
 
-                button.setOnClickListener{
-                editText1.setText("testees")
-            }
+        button.setOnClickListener {
+            editText1.setText("testees")
+        }
     }
 
 
